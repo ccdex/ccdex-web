73163f4d3abb4e21e4c5819ae0038e99f4330e39
xvalueorg <48717551+xvalueorg@users.noreply.github.com>
2019-05-03 09:50:37 +0800

    Update zh.js

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -15,27 +15,27 @@ module.exports = {
         btn2: '创建钱包'
     },
     section2:{
-        title:'产品/组件介绍 DEX + 交易协议 + 公链',
+        title:'核心组件 DEX + 交易协议 + 公链',
         label1:'跨链去中心化交易所',
         label1_des1:'·支持主流加密数字资产',
         label1_des2:'·友好的用户体验',
         label2:'新一代交易协议',
-        label2_des1:'·高性能内存撮合挖矿算法',
-        label2_des2:'·DEX间流动性共享机制',
-        label3:'全新区块链交易底座',
-        label3_des1:'·去中心化跨链协议(DCCP)',
+        label2_des1:'·分布式内存撮合引擎',
+        label2_des2:'·交易所流动性共享机制',
+        label3:'全新区块链交易底层平台',
+        label3_des1:'·去中心化跨链协议',
         label3_des2:'·易扩展的二层网络架构',
         btn:'查看详情'
     },
 
     section3:{
-        title:'去中心化跨链协议DCC Protocal',
+        title:'去中心化跨链协议 DCC Protocol',
         label1:'技术特点',
-        label1_des1:'- 基于MPC算法的安全可证方案',
-        label1_des2:'- 在区块链核心层实现跨链协议',
+        label1_des1:'- 基于MPC算法的可证明安全方案',
+        label1_des2:'- 位于区块链核心的跨链协议',
         label2:'应用优势',
         label2_des1:'- 支持超过上千种主流数字资产',
-        label2_des2:'- 缝适配多类型目标区块链',
+        label2_des2:'- 无缝适配不同类型区块链',
     
     },
 
@@ -45,16 +45,16 @@ module.exports = {
         label1_des1:'- 与区块周期匹配的集合竞价撮合算法',
         label1_des2:'- 以成交总量最大化为首要撮合规则',
         label2:'应用优势',
-        label2_des1:'- 高性能撮合挖矿',
+        label2_des1:'- 高性能交易撮合',
         label2_des2:'- 杜绝抢先交易确保交易公平性',
     },
     section5:{
-        title:' 交易所流动性共享',
+        title:' 交易所流动性共享机制',
         label1:'技术特点',
         label1_des1:'- 交易所共享订单池 Order Sharing Pool',
         label1_des2:'- 基于帕累托最优的订单共享激励机制',
         label2:'应用优势',
-        label2_des1:'- 跨链分布式交易所联盟及生态（DEX、钱包、量化）',
+        label2_des1:'- 跨链去中心化交易所联盟及生态（DEX、钱包、量化等）',
         label2_des2:'- 丰富的数字资产交易应用',
     },
     section6:{
